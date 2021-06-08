7a0d274b33c18b24d4407b8e908664a17f299a58
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-06-08 16:51:40 -0700

    Added Category Controller and Views

---
+++ WebAppNetCore/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppNetCore.Data;
+
+namespace WebAppNetCore.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+
+		public CategoryController(ApplicationDbContext db)
+		{
+			_db = db;
+
+		}
+
+		public IActionResult Index()
+		{
+			IEnumerable<Models.Category> objList = _db.Category;
+			return View(objList);
+		}
+
+
+		public IActionResult Create()
+		{
+			IEnumerable<Models.Category> objList = _db.Category;
+			return View(objList);
+		}
+
+
+	}
+}
